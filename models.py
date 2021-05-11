e8634d866d5c2cd8e4eb843162156cb3b95fff2f
jeroent99 <jeroen1749@gmail.com>
2021-05-11 22:38:22 +0200

    also update models

--- models.py
+++ models.py
@@ -1,5 +1,18 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from transformers import BertModel
+
+
+class Phi(nn.Module):
+
+    def __init__(self, W, b):
+        super(Linear, self).__init__()
+        self.weight = W
+        self.bias = b
+
+    def forward(self, input):
+        return F.linear(input, self.weight, self.bias)
 
 
 class Classifier(nn.Module):
@@ -8,7 +21,8 @@ class Classifier(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.encoder = BertModel.from_pretrained("bert-base-uncased")
-        self.mlp = nn.Linear(768, config['n_classes'])
+        self.gamma = None
+        self.phi = None
 
         if config['freeze_bert']:
             self.freeze_BERT()
@@ -22,11 +36,32 @@ class Classifier(nn.Module):
         x = self.encoder(x, 
                 token_type_ids=token_type_ids, 
                 attention_mask=attention_mask)["last_hidden_state"][:,0,:]
-        return self.mlp(x)
+        return self.phi(x)
 
-    def init_mlp(self):
-        """Possibly useful for the PROTO-MAML implementation, will remove if not."""
-        raise NotImplementedError
+    def init_phi(self, n_classes):
+        self.phi = nn.Linear(768, n_classes)
+        self.phi.weight = nn.Parameter(2 * self.gamma)
+        self.phi.bias = nn.Parameter(-torch.sum(self.gamma * self.gamma, dim=-1))
+
+        # detaching initialization
+        self.phi.weight.data = self.phi.weight.data.detach()
+        self.phi.bias.data = self.phi.bias.data.detach()
+
+    def replace_phi(self):
+        
+        """For evaluation of the inner model on the evaluation set we dont need to update
+        these parameters. Changing them from nn.Parameter to regular tensors allows the gradient
+        to flow through initialization (using nn.Parameter does not).
+        """
+        w_data = self.phi.weight.data
+        b_data = self.phi.bias.data
+
+        del self.phi.weight
+        del self.phi.bias
+
+        self.phi.weight = 2 * self.gamma + (w_data - 2 * self.gamma).detach()
+        scalar_norm = -torch.sum(self.gamma * self.gamma, dim=-1)
+        self.phi.bias = scalar_norm + (b_data - scalar_norm).detach()
 
 
 
